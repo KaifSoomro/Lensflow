be3372fa32e31b9d7164cf9288ab5b3bb5f602e6
KaifSoomro <kaifsoomro82@gmail.com>
2026-07-09 03:34:23 +0500

    Protected Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,14 @@ import Layout from "./layouts/Layout";
 import Illustrations from "./pages/illustrations";
 import Collections from "./pages/Collections";
 import DownloadHistory from "./pages/DownloadHistory";
-import Bookmarks from "./pages/Bookmarks"
+import Bookmarks from "./pages/Bookmarks";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import VerifyPage from "./pages/VerifyPage";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+import Notifications from "./pages/Notifications";
+import ProtectRoutes from "./components/common/ProtectRoutes";
 
 const App = () => {
   return (
@@ -17,13 +21,18 @@ const App = () => {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/illustrations" element={<Illustrations />} />
-          <Route path="/collections" element={<Collections />} />
-          <Route path="/download-history" element={<DownloadHistory />} />
-          <Route path="/bookmarks" element={<Bookmarks />} />
+          <Route element={<ProtectRoutes />}>
+            <Route path="/collections" element={<Collections />} />
+            <Route path="/download-history" element={<DownloadHistory />} />
+            <Route path="/bookmarks" element={<Bookmarks />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/notifications" element={<Notifications />} />
+          </Route>
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/verify/:token" element={<VerifyPage />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </>
   );
