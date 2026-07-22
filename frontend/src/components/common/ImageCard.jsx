0bf577f4b9be0d99114c8d2e908c3f4951c852c1
KaifSoomro <kaifsoomro82@gmail.com>
2026-07-22 13:23:56 +0500

    Image card

--- frontend/src/components/common/ImageCard.jsx
+++ frontend/src/components/common/ImageCard.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { Check } from "lucide-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
-const ImageCard = () => {
+const ImageCard = ({ value }) => {
   return (
-    <div>ImageCard</div>
-  )
-}
+    <Link
+      to={`/photo/${value?._id}`}
+      className="group relative mb-7 block w-full overflow-hidden rounded-lg cursor-zoom-in break-inside-avoid"
+    >
+      <img
+        src={value?.image_url}
+        alt={`image-${value?._id}`}
+        className="block w-full h-auto object-cover transition-transform duration-200"
+        draggable={false}
+      />
 
-export default ImageCard
+      <div className="absolute inset-0 z-10 bg-black/20 opacity-0 transition-opacity duration-200 group-hover:opacity-100">
+        <div className="absolute bottom-0 left-0 right-0 p-4 flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <img
+              src={value?.user?.profileImage}
+              alt={value?.user?.fullName}
+              className="w-10 h-10 rounded-full object-cover"
+            />
+
+            <div>
+              <h3 className="text-white font-medium">
+                {value?.user?.fullName}
+              </h3>
+
+              {value?.available && (
+                <p className="text-gray-300 text-xs flex items-center gap-1">
+                  Available for hire
+                  <span className="w-3 h-3 rounded-full flex items-center justify-center bg-gray-300 text-gray-900">
+                    <Check size={10} />
+                  </span>
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ImageCard;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,52 @@
 import React from "react";
 import ContributeCardButton from "../components/common/ContributeCardButton";
+import { useSelector } from "react-redux";
+import ImageCard from "../components/common/ImageCard.jsx";
+import Image from "../assets/images/mock_1.jpg";
+import ImageTwo from "../assets/images/mock_2.jpg";
 
 const Home = () => {
+  const { user } = useSelector((state) => state.user);
+
+  const imageData = [
+    {
+      _id: 1,
+      user: {
+        fullName: "Kaif Soomro",
+        profileImage:
+          "https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg",
+        email: "kaifsoomro82@gmail.com",
+      },
+      image_url: Image,
+      available: false
+    },
+
+    {
+      _id: 2,
+      user: {
+        fullName: "Basit Soomro",
+        profileImage:
+          "https://static.vecteezy.com/system/resources/thumbnails/005/346/410/small/close-up-portrait-of-smiling-handsome-young-caucasian-man-face-looking-at-camera-on-isolated-light-gray-studio-background-photo.jpg",
+        email: "basitsoomro822@gmail.com",
+      },
+      image_url: ImageTwo,
+      available: true
+    },
+  ];
   return (
     <div className="max-w-7xl mx-auto">
-      <div className="mt-8">
-        <ContributeCardButton />
+      {user && (
+        <div className="mt-8">
+          <ContributeCardButton />
+        </div>
+      )}
+      <div className="mt-10 columns-3 gap-7">
+        {
+          imageData?.map((value, index) => (
+            <ImageCard key={index} value={value}/>
+          ))
+        }
       </div>
-      <div>{/* images */}</div>
     </div>
   );
 };
