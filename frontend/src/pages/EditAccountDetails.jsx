20bc7c6c430a6a39df46e75b06c922edbec1bc34
KaifSoomro <kaifsoomro82@gmail.com>
2026-08-24 22:20:27 +0500

    Enhance EditAccountDetails page with profile image upload and user information fields

--- frontend/src/pages/EditAccountDetails.jsx
+++ frontend/src/pages/EditAccountDetails.jsx
@@ -1,7 +1,10 @@
 import { Check } from "lucide-react";
-import React from "react";
+import { useSelector } from "react-redux";
+import ProfileImage from "../assets/images/profile.webp";
 
 const EditAccountDetails = () => {
+  const { user } = useSelector((state) => state.user);
+
   return (
     <div className="max-w-7xl mx-auto ms-130 px-20 mt-29">
       <div className="flex items-center justify-between">
@@ -11,9 +14,52 @@ const EditAccountDetails = () => {
         </span>
       </div>
 
-      <div className="w-full flex items-center">
-        <div>
-          <img src="" alt="" />
+      <div className="w-full flex items-start justify-between">
+        <div className="w-80 mt-10">
+          <div className="group cursor-pointer">
+            <img src={ProfileImage} alt="profile_img" className="w-33" />
+            <p className="mt-3 underline text-neutral-500 group-hover:text-neutral-800 text-sm ">
+              Change profile image
+            </p>
+          </div>
+          <div className="mt-7">
+            <h1 className="font-semibold">Badge</h1>
+            <p className="text-neutral-500 text-sm font-semibold">
+              You don't have any badges yet :(
+            </p>
+          </div>
+        </div>
+        <div className="w-full mt-10">
+          <div className="flex justify-center gap-5 bg-amber-200">
+            <div className="w-full">
+              <label>First name</label>
+              <input
+                type="text"
+                className="w-full border rounded-lg outline-none px-3 py-1.5 mt-1.5"
+              />
+            </div>
+            <div className="w-full">
+              <label>Last name</label>
+              <input
+                type="text"
+                className="w-full border rounded-lg outline-none px-3 py-1.5 mt-1.5"
+              />
+            </div>
+          </div>
+          <div className="mt-5">
+            <label>Email</label>
+            <input
+              type="email"
+              className="w-full border rounded-lg outline-none px-3 py-1.5 mt-1.5"
+            />
+          </div>
+          <div className="mt-5">
+            <label>Username <p>(only letters, numbers, and underscores)</p></label>
+            <input
+              type="text"
+              className="w-full border rounded-lg outline-none px-3 py-1.5 mt-1.5"
+            />
+          </div>
         </div>
       </div>
     </div>
